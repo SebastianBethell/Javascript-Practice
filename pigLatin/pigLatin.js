e800ca46b29628f7e8033f780edd00b5cdd2d1d1
Sebastian Bethell <sebbethell@gmail.com>
2018-07-24 20:54:56 +0000

    solved easy pig latin translator using substring

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,1 +1,13 @@
- var myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2);
+let strOut = "";
+
+//Convert a word to pig latin by taking its first letter, moving it to the end of the word, then adding "ay" after it all.
+
+for (let i = 0; i < myArgs.length; i++) {
+  let firstLetter = myArgs[i].substring(0,1);
+  let newWord = myArgs[i].substring(1);
+
+  strOut += " " + newWord + firstLetter + "ay";
+}
+
+console.log(strOut);
